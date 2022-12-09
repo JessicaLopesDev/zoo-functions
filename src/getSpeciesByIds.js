64bf7607d111fcded59bcfad52ae95dca1401f36
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-12-09 20:58:05 -0300

    testing

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) => {
-  if (ids === undefined) return [];
+  if (ids === undefined) return [''];
 
   return ids.map((item) => data.species.filter((animal) => animal.id === item));
 };
