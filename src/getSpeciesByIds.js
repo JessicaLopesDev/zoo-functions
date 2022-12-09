8090e15053ed4ed5e79cebb6f62207912db9596a
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-12-09 19:36:59 -0300

    Requisito 1 teste

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (ids === undefined) return [];
+
+  return ids.map((item) => data.species.filter((animal) => animal.id === item));
 };
 
 module.exports = getSpeciesByIds;
