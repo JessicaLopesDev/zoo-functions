359e7363b4c65681ae55244495fe0d59bd478b33
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-12-12 17:13:53 -0300

    Requisito 5 teste

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
+const getAllSpecies = () => {
+  const result = {};
+  data.species.forEach((element) => {
+    result[element.name] = element.residents.length;
+  });
+  return result;
+};
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  const getBySpecie = data.species.find((item) => item.name === animal.species);
+  const getBySex = getBySpecie.residents.filter((resident) => resident.sex === animal.sex);
+
+  if (!animal) getAllSpecies();
+
+  if (!animal.sex) {
+    return getBySpecie.residents.length;
+  }
+
+  return getBySex.length;
 };
 
 module.exports = countAnimals;
