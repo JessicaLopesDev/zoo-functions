ec340436b0c3ead79402e99998b0273e76342111
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-12-12 18:03:46 -0300

    Requisito 6 e 7 ok

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,14 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('recebendo argumentos como parâmetro, retorna o esperado', () => {
+    expect(handlerElephants('count')).toBe(4);
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+    expect(handlerElephants()).toBeUndefined();
+    expect(handlerElephants(2)).toBe('Parâmetro inválido, é necessário uma string');
+    expect(handlerElephants('location')).toBe('NW');
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+    expect(handlerElephants('availability')).not.toContain('Monday');
+  });
+});
