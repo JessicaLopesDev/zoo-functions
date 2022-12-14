1652a67767c96743903000eb41827d57ddeafc04
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-12-14 18:24:08 -0300

    Requsito 11 ok

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
+// Filtra as especies que o employee é responsável
+const speciesOfEmployee = (person) => data.species
+  .filter((specie) => person.responsibleFor.includes(specie.id));
+// Cria objeto com todos os employees
+const allEmployees = () => data.employees.map((person) => ({
+  id: person.id,
+  fullName: `${person.firstName} ${person.lastName}`,
+  species: speciesOfEmployee(person).map((item) => item.name),
+  locations: speciesOfEmployee(person).map((item) => item.location),
+}));
+// Validação do id
+const idValidation = (idOfEmployee) => {
+  const validId = data.employees.some((element) => element.id === idOfEmployee);
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+  if (validId) {
+    return allEmployees().find((element) => element.id.includes(idOfEmployee));
+  }
+  if (!validId) {
+    throw new Error('Informações inválidas');
+  }
+};
+// Validação do name
+const nameValidation = (nameOfEmployee) => {
+  const validName = data.employees.some((element) =>
+    nameOfEmployee === element.firstName || nameOfEmployee === element.lastName);
+
+  if (validName) {
+    return allEmployees().find((element) => element.fullName.includes(nameOfEmployee));
+  }
+  if (!validName) {
+    throw new Error('Informações inválidas');
+  }
+};
+
+const getEmployeesCoverage = (employee) => {
+  if (!employee) return allEmployees();
+
+  if (employee.name) return nameValidation(employee.name);
+
+  if (employee.id) return idValidation(employee.id);
 };
 
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -6,12 +6,10 @@ const getOldestFromFirstSpecies = (id) => {
   const firstSpecieOfEmployee = data.species
     .find((specie) => specie.id === employee.responsibleFor[0]);
 
-  const findOldest = firstSpecieOfEmployee.residents.reduce((acc, curr) =>
-    (acc.age > curr.age ? acc : curr));
+  const findOldest = firstSpecieOfEmployee.residents
+    .reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
 
   return Object.values(findOldest);
 };
-
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
