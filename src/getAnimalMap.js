9063c070628bb11e859a99a38c2c7331487cdd6b
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-12-18 15:20:27 -0300

    Requisito 14 ok, projeto finalizado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -4,42 +4,81 @@ const findSpecieByLocation = (initial) => data.species.filter((specie) =>
   specie.location === initial).map((item) => item.name);
 
 // Array com nomes dos residentes por especie
-const findSpecie = (element) => data.species.find((item) =>
-  item.name === element).residents.map((resident) => resident.name);
+const findSpecie = (element) => data.species.find((item) => item.name === element);
 
-// Cria objeto
-const createSpecieObj = (initial) => findSpecieByLocation(initial).map((element) => ({
-  [element]: findSpecie(element),
-}));
+const createResidentsArrayBySex = (element, sex) =>
+  findSpecie(element).residents.filter((resident) => resident.sex === sex).map((item) => item.name);
 
-// .map((element) => data.species.find((specie) => element === specie.name).residents.name)
+// Cria objeto da especie com residentes por sexo em ordem ou sem ordem
+const createObjectBySex = (initial, sex, sorted) => {
+  if (sorted) {
+    return findSpecieByLocation(initial).map((element) => ({
+      [element]: createResidentsArrayBySex(element, sex).sort(),
+    }));
+  }
+  return findSpecieByLocation(initial).map((element) => ({
+    [element]: createResidentsArrayBySex(element, sex),
+  }));
+};
 
-// const createObject = () => {
-//   const objResult = {
-//     NE: findSpecieByLocation('NE'),
-//     NW: findSpecieByLocation('NW'),
-//     SE: findSpecieByLocation('SE'),
-//     SW: findSpecieByLocation('SW'),
-//   };
-//   return objResult;
-// };
+const createAllResidentsArray = (specie) =>
+  findSpecie(specie).residents.map((resident) => resident.name);
 
-const createObjWithNames = () => {
-  const result = {
-    NE: createSpecieObj('NE'),
-    NW: createSpecieObj('NW'),
-    SE: createSpecieObj('SE'),
-    SW: createSpecieObj('SW'),
+// Cria objeto da especie com nomes dos residentes
+const createObjectBySpecie = (initial, sorted) => {
+  if (sorted) {
+    return findSpecieByLocation(initial).map((element) => ({
+      [element]: createAllResidentsArray(element).sort(),
+    }));
+  }
+  return findSpecieByLocation(initial).map((element) => ({
+    [element]: createAllResidentsArray(element),
+  }));
+};
+
+const createObject = () => {
+  const objResult = {
+    NE: findSpecieByLocation('NE'),
+    NW: findSpecieByLocation('NW'),
+    SE: findSpecieByLocation('SE'),
+    SW: findSpecieByLocation('SW'),
   };
+  return objResult;
+};
+
+// Cria objeto final
+const createObjWithNames = (sex, sorted) => {
+  let result = {};
+  if (sex) {
+    result = {
+      NE: createObjectBySex('NE', sex, sorted),
+      NW: createObjectBySex('NW', sex, sorted),
+      SE: createObjectBySex('SE', sex, sorted),
+      SW: createObjectBySex('SW', sex, sorted),
+    };
+  }
+  if (!sex) {
+    result = {
+      NE: createObjectBySpecie('NE', sorted),
+      NW: createObjectBySpecie('NW', sorted),
+      SE: createObjectBySpecie('SE', sorted),
+      SW: createObjectBySpecie('SW', sorted),
+    };
+  }
   return result;
 };
 
+// Função principal, valida se recebe parametro e se recebe, quais são.
 const getAnimalMap = (options) => {
-  // const { includeNames, sex, sorted } = options;
+  if (!options) return createObject();
 
-  if (options) return createObjWithNames();
+  const { includeNames, sex, sorted } = options;
+
+  if (includeNames) return createObjWithNames(sex, sorted);
+
+  if (!includeNames) return createObject();
 };
 
-console.log(getAnimalMap({ includeNames: true }));
+console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
